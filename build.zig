1568c3554dcd85d9a4443aca4217c8efad7c62f1
Stefan Partheymüller <s.partheymueller@initics.de>
2025-08-24 21:02:05 +0200

    refactor(build): cleanup build.zig

--- build.zig
+++ build.zig
@@ -3,17 +3,18 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const options = .{ .target = target, .optimize = optimize };
 
     //
     // Dependencies
     //
 
     // Zalgebra: Linear algebra library for games and real-time graphics.
-    const zalgebra_dep = b.dependency("zalgebra", .{ .target = target, .optimize = optimize });
+    const zalgebra_dep = b.dependency("zalgebra", options);
     const zalgebra_mod = zalgebra_dep.module("zalgebra");
     // Raylib: Graphics library.
     // This is only used for the example.
-    const raylib_dep = b.dependency("raylib_zig", .{ .target = target, .optimize = optimize });
+    const raylib_dep = b.dependency("raylib_zig", options);
     const raylib_mod = raylib_dep.module("raylib");
     const raylib_lib = raylib_dep.artifact("raylib");
 
@@ -44,19 +45,18 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-        .imports = &.{
-            .{ .name = "phiz", .module = mod },
-        },
         // Link against libc for raylib.
         .link_libc = true,
     });
     exe_mod.addImport("raylib", raylib_mod);
+    exe_mod.addImport("phiz", mod);
     exe_mod.linkLibrary(raylib_lib);
+
     const exe = b.addExecutable(.{ .name = "exe", .root_module = exe_mod });
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run the example");
     run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
@@ -76,7 +76,10 @@ pub fn build(b: *std.Build) void {
 
     const mod_tests = b.addTest(.{ .root_module = test_mod });
     const run_mod_tests = b.addRunArtifact(mod_tests);
-    const install_mod_tests = b.addInstallArtifact(mod_tests, .{ .dest_sub_path = "test" });
+    const install_mod_tests = b.addInstallArtifact(
+        mod_tests,
+        .{ .dest_sub_path = "test" },
+    );
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
